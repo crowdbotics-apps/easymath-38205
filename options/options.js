ad4e177ff51f73c8a2add54d1fb777e33a22618b
Crowdbotics <team@crowdbotics.com>
2023-01-06 16:02:33 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "easymath-38205",
+  url: "https://easymath-38205.botics.co",
+  api: "https://easymath-38205.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
